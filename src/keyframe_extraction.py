bd6ac048505eb9fa89ed02545efb41e85c173304
Marco Duca <dcumrc02a07d086r@studenti.unical.it>
2025-06-28 11:47:15 +0200

    Cleaned keyframe_extraction.py

--- src/keyframe_extraction.py
+++ src/keyframe_extraction.py
@@ -9,14 +9,18 @@ import argparse
 from TransNetV2.inference import predict_video
 from transformers import CLIPProcessor, CLIPModel
 from sklearn.cluster import DBSCAN
-import faiss
 
+# Constants
+MIN_KEYFRAME_DENSITY = 1 / 10  # at least 1 keyframe every 10 sec
+FALLBACK_INTERVAL_SEC = 5 # read frame every 5 sec
+# End of constants
+
+# tries to extract frames in this order: target, target+1, target-1, target+2, target-2, ...
 def fetch_nearest_decodable_frame(cap: cv2.VideoCapture,
                                   target: int,
                                   start: int,
                                   end: int,
                                   max_probe: int = 15) -> tuple[bool, np.ndarray]:
-    # order: target, target+1, target-1, target+2, target-2, ...
     offsets = [0]
     for d in range(1, max_probe + 1):
         offsets.extend([d, -d])
@@ -37,16 +41,12 @@ def process_video(video_path: str,
                   CLIP_model, processor, 
                   fallback_enabled: bool = False,):
     try:
-        # Derive a simple ID for the video (file name without suffix)
         video_name = os.path.splitext(os.path.basename(video_path))[0]
-        # list of metadata that will be returned
         segment_metadata = []
-        
-        # Creating a per-video directory
         video_keyframe_dir = os.path.join(keyframe_dir, video_name)
         os.makedirs(video_keyframe_dir, exist_ok=True)
         
-        # TransNetV2 returns an (N, 2) array of [start_frame, end_frame] for every detected shot
+        # Calling TransNet for shot boundary detection
         with torch.no_grad():
             shots = np.array(predict_video(video_path, model))
             
@@ -55,66 +55,68 @@ def process_video(video_path: str,
         if not cap.isOpened():
             raise FileNotFoundError(f"Could not open {video_path}")
         fps = cap.get(cv2.CAP_PROP_FPS)
-    
         frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         duration_sec = frame_count / fps
         print()
         print(f"Processing {video_name}: {frame_count} frames, {len(shots)} shots detected.")
-
-            
-        # Fallback if keyframe density is too low
-        MIN_KEYFRAME_DENSITY = 1 / 10  # at least 1 every 10 sec
-        FALLBACK_INTERVAL_SEC = 5 # read image every 5 sec
         actual_density = len(shots) / duration_sec
         
-        if fallback_enabled and actual_density < MIN_KEYFRAME_DENSITY:
+        if fallback_enabled and actual_density < MIN_KEYFRAME_DENSITY: # fallback if keyframe density is too low
+            
             print(f" Low keyframe density ({actual_density:.4f} keyframes/sec). Triggering fallback...")
             cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
             interval_frames = int(FALLBACK_INTERVAL_SEC * fps)
             fallback_imgs   = []   
             fallback_frames = [] 
             
+            # select frames every FALLBACK_INTERVAL_SEC seconds
             for frame_idx in range(0, frame_count, interval_frames):
                 
                 cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
                 ret, frame = cap.read()
                 if not ret:
+                    # keep going if problems reading
                     continue
 
                 frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 img = Image.fromarray(frame_rgb)
                 fallback_imgs.append(img)  
                 fallback_frames.append(frame_idx)
-        
+
+            # clustering phase of fallback: create clusters from keyframes and, for each, 
+            # extract one (representative) keyframe
+            
+            # using CLIP to get embedding of each "fallback" keyframe
             inputs = processor(images=fallback_imgs, return_tensors="pt", padding=True)
             with torch.no_grad():
                 feats = CLIP_model.get_image_features(**inputs)
-            
             feats = feats / feats.norm(dim=-1, keepdim=True)
             embeddings = feats.cpu().numpy().astype('float32')
             
+            # clustering with DBSCAN using cosine similarity
             labels = DBSCAN(eps=0.1, min_samples=1, metric='cosine').fit_predict(embeddings)
-            
-            unique_lbls = [l for l in set(labels) if l != -1] # skip noise/outliers
+            # skip noise/outliers
+            unique_lbls = [l for l in set(labels) if l != -1] 
             repr_idx = []               
 
+            # iterate each cluster
             for lbl in unique_lbls:
                 idx   = np.where(labels == lbl)[0]  
                 embs  = embeddings[idx]               
 
-                # find cluster centroid 
+                # find centroid 
                 centroid = embs.mean(axis=0, keepdims=True)
                 centroid /= np.linalg.norm(centroid, axis=1, keepdims=True)
 
-                # cosine similarities to the centroid  (dot product because all unit-norm)
+                # find keyframe more similar to centroid
                 sims = (embs @ centroid.T).ravel()
-
                 best_local = idx[sims.argmax()]
                 repr_idx.append(best_local)
 
             repr_idx.extend(np.where(labels == -1)[0])
             
             idx = 0
+            # save results
             for local_i in repr_idx:
                 img = fallback_imgs[local_i]
                 fr_no = fallback_frames[local_i]
@@ -133,14 +135,16 @@ def process_video(video_path: str,
                     "keyframe_path": path,
                 })
                 idx+=1
-        else:
+                
+        else: # normal flow
+            
             # Iterate over every shot boundary pair
             for idx, (start_frame, end_frame) in enumerate(shots):     
                 
-                # Rigth now using middle frame as keyframe, could be improved
+                # using middle frame as keyframe
                 mid_frame = int(start_frame + ((end_frame - start_frame) // 2))
                 
-                # Skip if TransNetV2 produced an out-of-range index
+                # Skip if TransNet produced an out-of-range index
                 if mid_frame >= frame_count:
                     print(f"mid-frame {mid_frame} beyond EOF")
                     continue
@@ -148,22 +152,16 @@ def process_video(video_path: str,
                 # Find the middle frame and read it
                 cap.set(cv2.CAP_PROP_POS_FRAMES, mid_frame)
                 
-                # Try mid-frame first; fall back if necessary
+                # try first to extract middle frame, move around it if there are problems
                 ret, frame = fetch_nearest_decodable_frame(
                                 cap, target=mid_frame,
                                 start=start_frame, end=end_frame)
                 
-                #  fallback: step back 1 frame once
-                if not ret and mid_frame > start_frame:
-                    cap.set(cv2.CAP_PROP_POS_FRAMES, mid_frame - 1)
-                    ret, frame = cap.read()
-                    
                 if not ret:
                     print(f"Could not grab any frame in shot {idx}")
                     continue
-
                 
-                # From OpenCV to RGB to PIL.Image
+                # converting BGR to RGB to ensure correct colors
                 frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 img = Image.fromarray(frame_rgb)
                 
@@ -172,7 +170,7 @@ def process_video(video_path: str,
                 fpath = os.path.join(video_keyframe_dir, fname)
                 img.save(fpath, quality=95)
 
-                # Assemble per-shot metadata
+                # per-shot metadata
                 segment_metadata.append({
                     "video_id": video_name,
                     "shot": idx,
@@ -182,7 +180,6 @@ def process_video(video_path: str,
                     "end_time": float(end_frame   / fps),
                     "keyframe_path": fpath,
                 })
-        # Free the video file handle
         cap.release()
 
     except Exception as e:
@@ -191,19 +188,14 @@ def process_video(video_path: str,
 
 
 if __name__ == "__main__":
-    # CLI arguments
-    #   -i / --input   : video file or directory containing *.mp4
-    #   -o / --output  : root folder for key-frames & metadata
-    #   -fl/ --fallback : extract keyframes with a time-based subsampling
     parser = argparse.ArgumentParser(description="Video preprocessing: shot detection and keyframe extraction")
-    parser.add_argument("--input", "-i", required=True, help="Path to input video file or directory of videos")
-    parser.add_argument("--output", "-o", default="output", help="Directory to store output segments and keyframes")
-    parser.add_argument("--fallback", "-fl", default=True, help="Boolean to enable fallback time-based keyframe sampling if density is too low")
+    parser.add_argument("-i", required=True, help="Path to input video file or directory of videos")
+    parser.add_argument("-o", default="output", help="Directory to store output segments and keyframes")
+    parser.add_argument("-fl", default=True, help="Boolean to enable fallback time-based keyframe sampling if density is too low")
     args = parser.parse_args()
-    
     input_path = args.input
     
-    # Output folder structure:
+
     #   <output>/keyframes/ will contains per-video sub-folders
     #   <output>/metadata/  will contains per-video JSON files
     metadata_path = os.path.join(args.output, "metadata")
@@ -222,16 +214,22 @@ if __name__ == "__main__":
     
 
     if os.path.isdir(input_path):
+        
+        #Iterate on all videos in the directory
         for fname in os.listdir(input_path):
+            
             if fname.lower().endswith(".mp4"):
                 video_path = os.path.join(input_path, fname)
+                #Extract keyframes for single video
                 segment_metadata = process_video(video_path, keyframe_path, model, CLIP_model, processor, args.fallback)
                 
                 video_name = os.path.splitext(fname)[0]
                 meta_file  = os.path.join(metadata_path, f"{video_name}.json")
                 with open(meta_file, "w") as f:
                     json.dump(segment_metadata, f, indent=2)
-    else:
+                    
+    else: # processing a file and not a directory of videos
+        
         segment_metadata = process_video(args.input, keyframe_path, model, CLIP_model, processor, args.fallback)
         video_name = os.path.splitext(os.path.basename(input_path))[0]
         meta_file = os.path.join(metadata_path, f"{video_name}.json")
